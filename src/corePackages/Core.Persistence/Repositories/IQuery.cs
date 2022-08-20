9ea327e6e8e4f8611472511d03b1c948e5ba3036
okan çivgin <okanbjkspd@hotmail.com>
2022-08-21 01:34:55 +0300

    Add project files.

---
+++ src/corePackages/Core.Persistence/Repositories/IQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Persistence.Repositories;
+
+public interface IQuery<T>
+{
+
+    //Kendi sorgularımızı yazabildiğimiz , store producure lerimizi çalıştırabileceğimiz yapıları 
+    //yapabiliriz IQueryable sayesinde.Bu yapıyı o yüzden oluşturduk.
+    IQueryable<T> Query();
+}
---
+++ src/demoProjects/rentACar/Persistence/Repositories/BrandRepository.cs
@@ -0,0 +1,24 @@
+﻿using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Domain.Entities;
+using Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+
+    //Kulllanıcı markaya göre , renk e göre filtrelemeler yapabilir.Bunun kodları corePackages altında Dynamic kla
+    //söründe bulunmaktadır.Aynı zamanada sayfalama için yapı da bulunmaktadır.Paging kısmında
+    //Repositroies klasöründe hem asenkronlu hem asenkronsuz repositories de vardır
+    public class BrandRepository : EfRepositoryBase<Brand, BaseDbContext>, IBrandRepository
+    {
+        public BrandRepository(BaseDbContext context) : base(context)
+        {
+        }
+        
+    }
+}
